ccd965854ca57bdcfbb4785d18fa79bf2b030c56
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2014-05-26 11:07:50 +0200

    fix(miwg): update to new test layout

--- test/spec/miwg/MiwgSpec.js
+++ test/spec/miwg/MiwgSpec.js
@@ -8,13 +8,8 @@ describe('bpmn-miwg', function() {
   var helper = new Helper('bpmn-miwg', '../bpmn-miwg-test-suite');
 
 
-  it('should execute A testsuite', function(done) {
-    helper.runSuite('A - Fixed Digrams with Variations of Attributes/Reference/*.bpmn', done);
-  }, 20000);
-
-
-  it('should execute B testsuite', function(done) {
-    helper.runSuite('B - Validate that tool covers conformance class set/Reference/*.bpmn', done);
+  it('should execute testsuite', function(done) {
+    helper.runSuite('Reference/*.bpmn', done);
   }, 20000);
 
 });
