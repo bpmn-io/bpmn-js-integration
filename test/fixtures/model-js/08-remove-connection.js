ae34108da730f8fb795fd5bb14e8b17b80a5bce8
jdotzki <joerg.dotzki@zalando.de>
2014-08-27 15:00:46 +0200

    test(validation): test remove cases
    see bpmn-io/bpmn-js#106

---
+++ test/fixtures/model-js/08-remove-connection.js
@@ -0,0 +1,23 @@
+/* embedded and executed in browser test */
+
+// Test remove connection
+function executeTest(cli) {
+
+  // elements
+  var elements = cli.elements();
+
+  // element
+  var startEventElement = cli.element('StartEvent_1');
+
+  var gatewayShape1 = cli.append('StartEvent_1', 'bpmn:ExclusiveGateway', '150,0');
+
+  var task = cli.append(gatewayShape1, 'bpmn:Task', '250,0');
+
+  var gatewayShape2 = cli.append(task, 'bpmn:ExclusiveGateway', '0,100');
+
+  var connection = cli.connect(gatewayShape2, gatewayShape1, 'bpmn:SequenceFlow');
+
+  cli.append(gatewayShape2, 'bpmn:EndEvent', '100,0');
+
+  cli.removeConnection(connection);
+}
---
+++ test/fixtures/model-js/09-undo-remove.js
@@ -0,0 +1,27 @@
+/* embedded and executed in browser test */
+
+// Test remove and undo
+function executeTest(cli) {
+
+  // elements
+  var elements = cli.elements();
+
+  // element
+  var startEventElement = cli.element('StartEvent_1');
+
+  var gatewayShape1 = cli.append('StartEvent_1', 'bpmn:ExclusiveGateway', '150,0');
+
+  var task = cli.append(gatewayShape1, 'bpmn:Task', '250,0');
+
+  var gatewayShape2 = cli.append(task, 'bpmn:ExclusiveGateway', '0,100');
+
+  var connection = cli.connect(gatewayShape2, gatewayShape1, 'bpmn:SequenceFlow');
+
+  cli.append(gatewayShape2, 'bpmn:EndEvent', '100,0');
+
+  cli.removeShape(task);
+  cli.removeConnection(connection);
+
+  cli.undo();
+  cli.undo();
+}
