4563cfbe938c3ee6cc30cebc246c271bf01fd64a
Martin Stamm <martin.stamm@camunda.com>
2022-02-28 17:45:20 +0100

    chore: test modeling in collapsed subprocess

---
+++ test/spec/modeling/collapsed-subprocess/group.js
@@ -0,0 +1,13 @@
+/* @bpmn subprocess.bpmn */
+
+function executeTest(cli) {
+
+  cli.setRoot('SubProcess_1_plane');
+
+  cli.snapshot('plane_switched');
+
+  var planeElement = cli.element('SubProcess_1_plane');
+
+  var group = cli.create('bpmn:Group', '250,218', planeElement);
+
+}
