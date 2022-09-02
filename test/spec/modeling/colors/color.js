b4134d82bde7936e0ccac84b49d38bb1a33a8de5
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2022-09-02 14:01:15 +0200

    chore(color): temporarily mitigate bpmn-io/bpmn-js#1734
    The issue was not detected before the migration.
    Revert this commit as soon as https://github.com/bpmn-io/bpmn-js/issues/1734
    is fixed.

--- test/spec/modeling/colors/color.js
+++ test/spec/modeling/colors/color.js
@@ -6,6 +6,11 @@ async function executeTest(cli) {
 
   var elements = cli.elements();
 
+  // TODO(@barmac): remove this once bpmn-js#1734 is fixed (https://github.com/bpmn-io/bpmn-js/issues/1734)
+  elements = elements.filter(function(element) {
+    return !/Process|Collaboration/i.test(element);
+  });
+
   // reset colors
   cli.color(elements, 'unset,unset');
 
