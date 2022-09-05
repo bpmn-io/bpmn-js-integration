6c02c79e95a0bc457cddd146e37ea19e331e2b94
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2022-09-05 09:23:42 +0200

    test: remove workaround for bpmn-io/bpmn-js#1734
    Related to https://github.com/bpmn-io/bpmn-js/issues/1734

--- test/spec/modeling/colors/color.js
+++ test/spec/modeling/colors/color.js
@@ -6,11 +6,6 @@ async function executeTest(cli) {
 
   var elements = cli.elements();
 
-  // TODO(@barmac): remove this once bpmn-js#1734 is fixed (https://github.com/bpmn-io/bpmn-js/issues/1734)
-  elements = elements.filter(function(element) {
-    return !/Process|Collaboration/i.test(element);
-  });
-
   // reset colors
   cli.color(elements, 'unset,unset');
 
