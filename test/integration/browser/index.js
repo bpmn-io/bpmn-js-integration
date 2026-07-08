2939af2294950f9926c34e33f4588d2c97d88797
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-07-08 14:11:33 +0200

    chore: drop puppeteer + use local chrome

--- test/integration/browser/index.js
+++ test/integration/browser/index.js
@@ -1,16 +1,40 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
+const which = require('which');
 
 // eslint-disable-next-line no-unused-vars
 function _log(err, message) {
   console.log('  ' + (err ? 'ERR! ' : '') + message.toString('utf-8').replace(/\n\s*$/, ''));
 }
 
+async function findChrome() {
+
+  if (process.env.CHROME_BIN) {
+    return process.env.CHROME_BIN;
+  }
+
+  const names = [ 'google-chrome', 'chrome', 'chromium' ];
+
+  for (const name of names) {
+    try {
+      return await which(name);
+    } catch {
+      continue;
+    }
+  }
+
+  throw new Error('chrome not found via: ' + names.join(', '));
+}
+
 let browser;
 
 before(async function openBrowser() {
-  this.timeout(5000);
+  this.timeout(10000);
+
+  const executablePath = await findChrome();
+
   browser = await puppeteer.launch({
-    headless: 'new'
+    headless: 'new',
+    executablePath
   });
 });
 
